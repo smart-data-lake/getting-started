729eade12b2dda0cfd71ffffafd929580bb60660
Ilias Soto <iliassoto@hotmail.com>
2023-05-15 11:36:05 +0200

    add envConfig to sdlb visualizer

--- start-visualizer-windows.cmd
+++ start-visualizer-windows.cmd
@@ -1,4 +1,5 @@
 rmdir /s /Q "%~dp0sdlb-viewer-windows\config"
 Xcopy /E /I "%~dp0config" "%~dp0sdlb-viewer-windows\config"
+Xcopy /E /I "%~dp0envConfig" "%~dp0sdlb-viewer-windows\config"
 cd "%~dp0sdlb-viewer-windows"
 .\lighttpd\lighttpd.exe -D -f .\lighttpd.conf
