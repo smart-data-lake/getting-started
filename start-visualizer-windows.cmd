51d6e96ebbc132b7cf9c441337c55daddb75f840
Ilias Soto <iliassoto@hotmail.com>
2023-05-15 11:12:50 +0200

    sdlb-viewer for windows server

---
+++ start-visualizer-windows.cmd
@@ -0,0 +1,4 @@
+rmdir /s /Q "%~dp0sdlb-viewer-windows\config"
+Xcopy /E /I "%~dp0config" "%~dp0sdlb-viewer-windows\config"
+cd "%~dp0sdlb-viewer-windows"
+.\lighttpd\lighttpd.exe -D -f .\lighttpd.conf
