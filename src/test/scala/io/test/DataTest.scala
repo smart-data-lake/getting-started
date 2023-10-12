b4da6b7b9ff4e310998b2a7597b02ff6b2c05ee4
tbb <timo.babst@elca.ch>
2023-10-12 11:41:05 +0200

    remove timestamps in departures.conf

--- src/test/scala/io/test/DataTest.scala
+++ src/test/scala/io/test/DataTest.scala
@@ -10,10 +10,20 @@ class DataTest extends FunSuite {
     .appName("data explorer")
     .getOrCreate()
 
-  test("prints table") {
-    val df_airports = spark.read.parquet("./data/int_departures")
-    df_airports.cache()
-    df_airports.show()
-    print("### int_departures has currently ", df_airports.count(), " number of rows")
+  test("read deltalake vacuum and history") {
+    import io.delta.tables._
+    val deltaTable = DeltaTable.forPath(spark, "data/int_departures")
+    //spark.conf.set("spark.databricks.delta.retentionDurationCheck.enabled", "false")
+    //deltaTable.vacuum(0).show(false)
+    //deltaTable.history().show(false)
+    deltaTable.toDF.show()
+  }
+  test("read deltalake count") {
+    import io.delta.tables._
+    val deltaTable = DeltaTable.forPath(spark, "data/int_departures")
+    while (true) {
+      Thread.sleep(10000)
+      println(" int_departures has currently" + deltaTable.toDF.count() + " number of rows")
+    }
   }
 }
