4523fece1a3929b0e9cdb3c4a4ac5481767059c9
Geheiner <timo.babst@gmail.com>
2023-10-27 14:54:24 +0200

    Update part3/src/main/scala/io/smartdatalake/workflow/dataobject/CustomWebserviceDataObject.scala
    Co-authored-by: Thierry Girod <thierry.girod@hotmail.com>

--- part3/src/main/scala/io/smartdatalake/workflow/dataobject/CustomWebserviceDataObject.scala
+++ part3/src/main/scala/io/smartdatalake/workflow/dataobject/CustomWebserviceDataObject.scala
@@ -25,7 +25,7 @@ import scala.util.{Failure, Success}
 case class HttpTimeoutConfig(connectionTimeoutMs: Int, readTimeoutMs: Int)
 
 // Default to the interval of [2 weeks and 2 days ago] -> [2 weeks ago]
-case class DepartureQueryParameters(airport: String, begin: Long = System.currentTimeMillis()/1000 - 1209600 - 172800L, end: Long = System.currentTimeMillis()/1000 - 1209600 )
+case class DepartureQueryParameters(airport: String, begin: Long = System.currentTimeMillis() / 1000 - 1209600 - 172800L, end: Long = System.currentTimeMillis() / 1000 - 1209600 )
 
 /**
  * [[DataObject]] to call webservice and return response as a DataFrame
