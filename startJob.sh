617c35f1cfd2247d8d220bde605b28415a37afbd
zkull <zach.kull@gmail.com>
2024-03-11 09:19:28 +0100

    add updateSchemaStats.sh

--- startJob.sh
+++ startJob.sh
@@ -8,5 +8,5 @@ fi
 
 mkdir -p ./data
 
-set -f # disable the glob
-podman run --rm --name=sdlb-job -v ${PWD}/data:/mnt/data -v ${PWD}/target:/mnt/lib -v ${PWD}/config:/mnt/config -v ${PWD}/viz/description:/mnt/description $ENTRYPOINT sdl-spark:latest $@
+set -f # disable globs
+podman run --rm --name=sdlb-job -v ${PWD}/data:/mnt/data -v ${PWD}/target:/mnt/lib -v ${PWD}/config:/mnt/config -v ${PWD}/viz/description:/mnt/description -v ${PWD}/viz/schema:/mnt/schema -e CLASS=$CLASS sdl-spark:latest $@
