6af285618c7e9b7aeb65d14f6d195dbe239433f3
zkull <zach.kull@gmail.com>
2024-03-12 08:31:34 +0100

    add sparkShell

---
+++ spark/install_spark.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# This script is intended for use from the docker builds.
+
+set -e
+
+${$SCALA_VERSION:?}
+${SPARK_VERSION:?}
+${HADOOP_VERSION:?}
+SPARK_VERSION_DIR="spark-${SPARK_VERSION}"
+if [ "$SCALA_VERSION" == "2.13" ]; then SCALA_POSTFIX="-scala2.13"; fi
+SPARK_NAME="spark-${SPARK_VERSION}-bin-hadoop${HADOOP_VERSION}${SCALA_POSTFIX}"
+
+pushd /opt
+rm -rf spark
+wget -q -O - "https://dlcdn.apache.org/spark/${SPARK_VERSION_DIR}/${SPARK_NAME}.tgz" | tar xvz --no-same-owner
+mv "${SPARK_NAME}" spark
+popd
+
+if test -z "${SPARK_DIST_CLASSPATH}"; then
+  echo "Skipping spark env"
+else
+  echo "export SPARK_DIST_CLASSPATH=\"${SPARK_DIST_CLASSPATH}\"" > /opt/spark/conf/spark-env.sh
+fi
