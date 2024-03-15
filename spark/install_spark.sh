7561637f5cacbdf82e69bb527c9bd506978a28c3
zkull <zach.kull@gmail.com>
2024-03-15 23:10:24 +0100

    fix script for executing all feeds (star expansion)

--- spark/install_spark.sh
+++ spark/install_spark.sh
@@ -3,9 +3,9 @@
 
 set -e
 
-${$SCALA_VERSION:?}
-${SPARK_VERSION:?}
-${HADOOP_VERSION:?}
+echo ${SCALA_VERSION:?}
+echo ${SPARK_VERSION:?}
+echo ${HADOOP_VERSION:?}
 SPARK_VERSION_DIR="spark-${SPARK_VERSION}"
 if [ "$SCALA_VERSION" == "2.13" ]; then SCALA_POSTFIX="-scala2.13"; fi
 SPARK_NAME="spark-${SPARK_VERSION}-bin-hadoop${HADOOP_VERSION}${SCALA_POSTFIX}"
