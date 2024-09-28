cc0c777b582202492d541b60046605637aae02eb
zkull <zach.kull@gmail.com>
2024-09-28 18:03:38 +0200

    refactor part 1 and 2

---
+++ shell.scala
@@ -0,0 +1,8 @@
+
+//println("loading SDLB Scala interface")
+
+//import io.smartdatalake.generated._
+//import io.smartdatalake.lab.SmartDataLakeBuilderLab
+
+//val sdlb = SmartDataLakeBuilderLab[DataObjectCatalog, ActionCatalog](spark,Seq("/mnt/config"), DataObjectCatalog(_, _), ActionCatalog(_, _))
+//implicit val context = sdlb.context
