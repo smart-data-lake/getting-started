d3e3495ec6b4f2ca7a3ba4ad491641bf6a12d2b3
zkull <zach.kull@gmail.com>
2024-10-06 22:49:11 +0200

    shell.scala: fix envConfig

--- shell.scala
+++ shell.scala
@@ -4,5 +4,5 @@
 //import io.smartdatalake.generated._
 //import io.smartdatalake.lab.SmartDataLakeBuilderLab
 
-//val sdlb = SmartDataLakeBuilderLab[DataObjectCatalog, ActionCatalog](spark,Seq("/mnt/config"), DataObjectCatalog(_, _), ActionCatalog(_, _))
+//val sdlb = SmartDataLakeBuilderLab[DataObjectCatalog, ActionCatalog](spark,Seq("/mnt/config","/mnt/envConfig/dev.conf"), DataObjectCatalog(_, _), ActionCatalog(_, _))
 //implicit val context = sdlb.context
