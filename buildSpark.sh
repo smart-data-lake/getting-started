1bd54f3cfb4a582f2084385a66d25cbf3e6960cf
tbb <timo.babst@elca.ch>
2024-10-09 15:49:57 +0200

    try to fix problems unsuccesfully

--- buildSpark.sh
+++ buildSpark.sh
@@ -4,4 +4,4 @@
 mkdir -p ~/.m2/repository
 
 # start maven build
-podman build -v ~/.m2/repository:/mnt/.mvnrepo -t sdl-spark -f spark/Dockerfile ..
+podman build -v ~/.m2/repository:/mnt/.mvnrepo -t sdl-spark -f spark/Dockerfile .
