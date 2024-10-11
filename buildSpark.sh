fa1566b64ec62eaeb803b1439c5d78e0015d6c8a
zkull <zach.kull@gmail.com>
2024-10-11 08:12:17 +0200

    fix install python 3.11

--- buildSpark.sh
+++ buildSpark.sh
@@ -6,4 +6,3 @@ mkdir -p ~/.m2/repository
 # start maven build
 # seccomp=unconfined is needed for current ubuntu 22.04, otherwise there "untar" has problems setting permissions for directories, see also https://bugs.launchpad.net/ubuntu/+source/tar/+bug/2059734 and https://github.com/ocaml/infrastructure/issues/121
 podman build --security-opt seccomp=unconfined -v ~/.m2/repository:/mnt/.mvnrepo -t sdl-spark -f spark/Dockerfile .
-buildSpark.sh
