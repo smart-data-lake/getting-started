bfa719a5f99f8c6f89661c725af388eec122812e
zkull <zach.kull@gmail.com>
2024-10-12 12:15:23 +0200

    some cleanup

--- InitSDLBInterface.scala
+++ InitSDLBInterface.scala
@@ -1,4 +1,6 @@
 import io.smartdatalake.generated._
 import io.smartdatalake.lab.SmartDataLakeBuilderLab
-val sdlb = SmartDataLakeBuilderLab[DataObjectCatalog, ActionCatalog](spark,Seq("/mnt/config"), DataObjectCatalog(_, _), ActionCatalog(_, _))
+
+println("Initializing SDLB Scala interface")
+val sdlb = SmartDataLakeBuilderLab[DataObjectCatalog, ActionCatalog](spark,Seq("/mnt/config","/mnt/envConfig/dev.conf"), DataObjectCatalog(_, _), ActionCatalog(_, _))
 implicit val context = sdlb.context
--- startJob.sh
+++ startJob.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-# prepare webservice fallback data
-if [ ! -d ./data/fallback-download ]; then
-  echo "copying webservice fallback data"
-  #cp -r ./data-fallback-download ./data/fallback-download TODO this generates an error, is this still up to date?
-fi
-
-mkdir -p ./data
-
 set -f # disable star expansion
 podman run --rm --name=sdlb-job -v ${PWD}/data:/mnt/data -v ${PWD}/target:/mnt/lib -v ${PWD}/config:/mnt/config -v ${PWD}/envConfig:/mnt/envConfig -v ${PWD}/viz/state:/mnt/state -v ${PWD}/viz/description:/mnt/description -v ${PWD}/viz/schema:/mnt/schema -e CLASS=$CLASS sdl-spark:latest $@
